ed7ad058fa49c1bc4494fc57cb52dc3e70bb6c22
Cho Won-eob <1up@1upz.com>
2015-01-05 18:51:09 +0900

    html 파싱 후 스크립트 제거를 위한 함수 추가

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
 var cheerio = require('cheerio');
 var request = require('request');
 
+function stripScripts(s) {
+	return s.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
+}
+
 module.exports = function(app) {
 	app.get('/', function(req, res) {
 		res.render('index');
