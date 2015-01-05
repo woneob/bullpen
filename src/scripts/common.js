cea36c333d1bd4b8557fe1824cf79cb62adb26b1
Cho Won-eob <1up@1upz.com>
2015-01-05 18:07:43 +0900

    공통 js 추가

---
+++ src/scripts/common.js
@@ -0,0 +1,24 @@
+var isNode = (typeof process !== 'undefined' && typeof require !== 'undefined');
+var isNodeWebkit = false;
+var gui;
+var win;
+
+//Is this Node.js?
+if(isNode) {
+  //If so, test for Node-Webkit
+  try {
+    isNodeWebkit = (typeof require('nw.gui') !== 'undefined');
+  } catch(e) {
+    isNodeWebkit = false;
+  }
+}
+
+(function($) {
+  if (isNodeWebkit) {
+    gui = require('nw.gui');
+    win = gui.Window.get();
+
+    win.resizeTo(600, 800);
+    win.setPosition('center');
+  }
+})(jQuery);
