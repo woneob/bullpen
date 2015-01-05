ab67529af94396f765e98339723fe545d43df621
Cho Won-eob <1up@1upz.com>
2015-01-05 15:40:53 +0900

    라우터 분리

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
-var app = express();
 var ejsengine = require('ejs-locals');
+
+var app = express();
+var routes = require('./config/routes');
 
 app.engine('ejs', ejsengine);
 app.set('views', __dirname + '/views');
@@ -8,9 +10,7 @@ app.set('view engine', 'ejs');
 app.set('port', 3801);
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/', function(req, res) {
-	res.send('hello');
-});
+routes(app);
 
 app.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
