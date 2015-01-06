9ad3a064d9bfedd1f8f7c3c68095cd228b072a22
Cho WonEob <1up@1upz.com>
2015-01-07 02:00:34 +0900

    인코딩 변환을 위해 iconv-lite 모듈 추가

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 var cheerio = require('cheerio');
 var request = require('request');
+var iconv = require('iconv-lite');
 
 function stripScripts(s) {
 	return s.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
